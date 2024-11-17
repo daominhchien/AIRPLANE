c1a5b96f8e2c4e2703e6c3439c21ccd1d5449364
Minh Chiến <127067959+daominhchien@users.noreply.github.com>
2024-11-17 18:38:46 +0700

    Update script_QLMB.js

--- script_QLMB.js
+++ script_QLMB.js
@@ -2,21 +2,23 @@ document.addEventListener('DOMContentLoaded', function() {
     const openModalButton = document.getElementById('openModalButton');
     const modalOverlay = document.getElementById('modalOverlay');
     const addPlaneButton = document.getElementById('addPlaneButton');
+    const closeModalButton = document.getElementById('closeModalButton'); // Nút đóng modal
 
     // Thiết lập sự kiện nhấn nút mở modal
     openModalButton.addEventListener('click', function() {
-        if (modalOverlay.style.display === 'block') {
-            modalOverlay.style.display = 'none';
-            document.body.style.overflow = 'auto'; // Bật lại cuộn trang
-        } else {
-            modalOverlay.style.display = 'block';
-            document.body.style.overflow = 'hidden'; // Ngăn cuộn trang
-        }
+        modalOverlay.style.display = 'flex'; // Hiển thị modal với display: flex;
+        document.body.style.overflow = 'hidden'; // Ngăn cuộn trang khi modal hiển thị
     });
 
     // Thiết lập sự kiện nhấn nút thêm máy bay
-    addPlaneButton.addEventListener('click', function() {   
+    addPlaneButton.addEventListener('click', function() {  
         modalOverlay.style.display = 'none'; // Đóng modal
         document.body.style.overflow = 'auto'; // Bật lại cuộn trang
+    });
+
+    // Sự kiện nhấn nút đóng modal (X)
+    closeModalButton.addEventListener('click', function() {
+       modalOverlay.style.display = 'none'; // Đóng modal
+       document.body.style.overflow = 'auto'; // Bật lại cuộn trang
     });
 });
